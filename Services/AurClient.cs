f226f8b81fef02d7e5cd57c073ce6ee8fb312198
aze <226987740+azecx@users.noreply.github.com>
2026-05-15 15:23:40 +0200

    serialization fix

--- Services/AurClient.cs
+++ Services/AurClient.cs
@@ -4,6 +4,15 @@ using System.Text.Json.Serialization;
 
 namespace Shitman
 {
+    public class AurResponse
+    {
+        [JsonPropertyName("results")]
+        public List<AurPackage> Results { get; set; }
+    }
+
+    [JsonSerializable(typeof(AurResponse))]
+    internal partial class AurJsonContext : JsonSerializerContext {}
+
     public class AurClient
     {
         static HttpClient client;
@@ -16,34 +25,21 @@ namespace Shitman
         public async Task<List<AurPackage>?> Search(string query)
         {
             string url = $"https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg={query}";
-
             string json = await client.GetStringAsync(url);
-
-            AurResponse response = JsonSerializer.Deserialize<AurResponse>(json);
-            
+            AurResponse? response = JsonSerializer.Deserialize(json, AurJsonContext.Default.AurResponse);
             return response?.Results;
         }
 
         public async Task<AurPackage?> GetPackage(string name)
         {
             string url = $"https://aur.archlinux.org/rpc/?v=5&type=info&arg[]={name}";
-
             string json = await client.GetStringAsync(url);
-
-            AurResponse? response = JsonSerializer.Deserialize<AurResponse>(json);
-
+            AurResponse? response = JsonSerializer.Deserialize(json, AurJsonContext.Default.AurResponse);
             if (response?.Results == null || response.Results.Count == 0)
-            {   
+            {
                 return null;
             }
-
             return response?.Results?.FirstOrDefault();
         }
-    }
-
-    public class AurResponse
-    {
-        [JsonPropertyName("results")]
-        public List<AurPackage> Results { get; set; }
     }
 }
